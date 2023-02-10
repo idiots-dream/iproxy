4681bb57856a4e6a73cb9b3a35bbebdb18273583
idiots-dream <703347383@qq.com>
2023-02-10 14:29:54 +0800

    init

---
+++ middleware.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func maxAllowedMiddleware(n uint) mux.MiddlewareFunc {
+	sem := make(chan struct{}, n)
+	acquire := func() { sem <- struct{}{} }
+	release := func() { <-sem }
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			acquire()
+			defer release()
+			next.ServeHTTP(w, r)
+		})
+	}
+}
